7ee8588b0cb5c2fbcb9329df81026d856295724a
sunchj8 <9082944828a@gmail.com>
2025-05-15 01:03:38 +0300

    Обновила пути к изображениям

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/VelvetRose/', 
   plugins: [vue()],
 })
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/VelvetRose/' : '/'
-}
